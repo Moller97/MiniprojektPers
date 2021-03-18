81414c6112b5a2b128f15b7b0ec63f51931386aa
Line <Linevilladsen@yahoo.dk>
2021-03-18 12:26:32 +0100

    Tøj

--- src/model/Clothing.java
+++ src/model/Clothing.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Clothing {
+public class Clothing extends Product {
+private String size;
+private String colour;
+
+public Clothing(String productName, int purchasePrice, int salesPrice, int rentPrice, String countryOfOrigin, int stockStorage, int productNo, int stockAmount, String storageName, String size, String colour) {
+super(productName, purchasePrice, salesPrice, rentPrice, countryOfOrigin, stockStorage, productNo, stockAmount, storageName);
+this.size = size;
+this.colour = colour;
+
+}
+
+public String getSize() {
+	return size;
+}
+
+public void setSize(String size) {
+	this.size = size;
+}
+
+public String getColour() {
+	return colour;
+}
+
+public void setcolour(String colour) {
+	this.colour = colour;
+}
+
+public String printProductClothing() {
+	return "\nProdukt navn: " + getProductName() + "\nKøbspris " + getPurchasePrice() + "\nSlagspris " + getSalesPrice() + "\nLejepris" + getRentPrice() + "\nOprindelsesland: " + getCountryOfOrigin() + "\nAntal tøj på dette lager: " + getStorageName() + getStockStorage() + "\nProduktNummer: " + getProductNo() + "\nAntal af tøj i alt: " + getStockAmount() + "\nStørrelse: " + getSize() + "\nFarve: " + getColour();
+
+}
 
 }
