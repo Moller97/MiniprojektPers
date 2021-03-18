161cb8a65c8c1eea82052ac2e8cd56f4b9cac1f7
Line <Linevilladsen@yahoo.dk>
2021-03-18 12:12:07 +0100

    produkt

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,7 +1,6 @@
 package model;
 
 public class Product {
-private String category;
 private String productName;
 private int purchasePrice;
 private int salesPrice;
@@ -10,10 +9,9 @@ private String countryOfOrigin;
 private int stockStorage;
 private int productNo;
 private int stockAmount;
-private String storage;  
+private String storageName;  
 
-public Product(String category, String productName, int purchasePrice, int salesPrice, int rentPrice, String countryOfOrigin, int stockStorage, int productNo, int stockAmount, String storage) {
-this.category = category;
+public Product(String productName, int purchasePrice, int salesPrice, int rentPrice, String countryOfOrigin, int stockStorage, int productNo, int stockAmount, String storageName) {
 this.productName = productName;
 this.purchasePrice = purchasePrice;
 this.rentPrice = rentPrice;
@@ -21,7 +19,85 @@ this.countryOfOrigin = countryOfOrigin;
 this.stockStorage = stockStorage;
 this.productNo = productNo;
 this.stockAmount = stockAmount;
-this.storage = storage;
+this.storageName = storageName;
+
+}
+
+public String getProductName() {
+	return productName;
+}
+
+public void setProductName(String productName) {
+	this.productName = productName;
+}
+
+public int getPurchasePrice() {
+	return purchasePrice;
+}
+
+public void setPurchasePrice(int purchasePrice) {
+	this.purchasePrice = purchasePrice;
+}
+
+public int getSalesPrice() {
+	return salesPrice;
+}
+
+public void setSalesPrice(int salesPrice) {
+	this.salesPrice = salesPrice;
+}
+
+public int getRentPrice() {
+	return rentPrice;
+}
+
+public void setRentPrice(int rentPrice) {
+	this.rentPrice = rentPrice;
+}
+
+public String getCountryOfOrigin() {
+	return countryOfOrigin;
+}
+
+public void setCountryOfOrigin(String countryOfOrigin) {
+	this.countryOfOrigin = countryOfOrigin;
+
+}
+
+public int getStockStorage() {
+	return stockStorage;
+}
+
+public void setStockStorage(int stockStorage) {
+	this.stockStorage = stockStorage;
+}
+
+public int getProductNo() {
+	return productNo;
+}
+
+public void setProductNo(int productNo) {
+	this.productNo = productNo;
+}
+
+public int getStockAmount() {
+	return stockAmount;
+}
+
+public void setStockAmount(int stockAmount) {
+	this.stockAmount = stockAmount;
+}
+
+public String getStorageName() {
+	return storageName;
+}
+
+public void setStorageName(String storageName) {
+	this.storageName = storageName;
+}
+
+public String printProduct() {
+	return "\nProdukt navn: " + getProductName() + "\nKøbspris " + getPurchasePrice() + "\nSlagspris " + getSalesPrice() + "\nLejepris" + getRentPrice() + "\nOprindelsesland: " + getCountryOfOrigin() + "\nAntal tøj på dette lager: " + getStorageName() + getStockStorage() + "\nProduktNummer: " + getProductNo() + "\nAntal af tøj i alt: " + getStockAmount();
 
 }
 
