bffcbb2b9e5d0bff208d4c32dd4162582c6b10a0
Line <Linevilladsen@yahoo.dk>
2021-03-18 12:38:55 +0100

    udstyr og pistol ting ting

--- src/model/Equipment.java
+++ src/model/Equipment.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Equipment {
+public class Equipment extends Product {
+private String type;
+private String description;
+
+public Equipment(String productName, int purchasePrice, int salesPrice, int rentPrice, String countryOfOrigin, int stockStorage, int productNo, int stockAmount, String storageName, String type, String description) {
+super(productName, purchasePrice, salesPrice, rentPrice, countryOfOrigin, stockStorage, productNo, stockAmount, storageName);
+this.type = type;
+this.description = description;
+	
+}
+
+public String getType() {
+	return type;
+}
+
+public void setType(String type) {
+	this.type = type;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public String printProductClothing() {
+	return "\nProdukt navn: " + getProductName() + "\nKøbspris " + getPurchasePrice() + "\nSlagspris " + getSalesPrice() + "\nLejepris" + getRentPrice() + "\nOprindelsesland: " + getCountryOfOrigin() + "\nAntal tøj på dette lager: " + getStorageName() + getStockStorage() + "\nProduktNummer: " + getProductNo() + "\nAntal af tøj i alt: " + getStockAmount() + "\nType: " + getType() + "\nBeskrivelse: " + getDescription();
+
+}
 
 }
--- src/model/GunReplica.java
+++ src/model/GunReplica.java
@@ -1,5 +1,35 @@
 package model;
 
-public class GunReplica {
+public class GunReplica extends Product {
+private String calibre;
+private String material;
+	
+public GunReplica(String productName, int purchasePrice, int salesPrice, int rentPrice, String countryOfOrigin, int stockStorage, int productNo, int stockAmount, String storageName, String calibre, String material) {
+super(productName, purchasePrice, salesPrice, rentPrice, countryOfOrigin, stockStorage, productNo, stockAmount, storageName);
+this.calibre = calibre;
+this.material = material;
+
+}
+
+public String getCalibre() {
+	return calibre;
+}
+
+public void setCalibre(String calibre) {
+	this.calibre = calibre;
+}
+
+public String getMaterial() {
+	return material;
+}
+
+public void setMaterial(String material) {
+	this.material = material;
+}
+
+public String printProductGunReplica() {
+	return "\nProdukt navn: " + getProductName() + "\nKøbspris " + getPurchasePrice() + "\nSlagspris " + getSalesPrice() + "\nLejepris" + getRentPrice() + "\nOprindelsesland: " + getCountryOfOrigin() + "\nAntal tøj på dette lager: " + getStorageName() + getStockStorage() + "\nProduktNummer: " + getProductNo() + "\nAntal af tøj i alt: " + getStockAmount() + "\nKlaiber: " + getCalibre() + "\nMateriale: " + getMaterial();
+
+}
 
 }
