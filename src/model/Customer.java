19c0160ed37f47b4cfae89ed486728ff6ee491ed
Line <Linevilladsen@yahoo.dk>
2021-03-18 10:05:31 +0100

    customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,68 @@
 package model;
 
 public class Customer {
+private int customerId;
+private String name;
+private String address;
+private int zipcode;
+private String city;
+private int phoneNo;
+
+public Customer(int customerId, String name, String address, int zipcode, String city, int phoneNo) {
+this.customerId = customerId;
+this.name = name;
+this.address = address;
+this.zipcode = zipcode;
+this.city = city;
+this.phoneNo = phoneNo;
+
+}
+public int getCustomerId() {
+	return customerId;
+}
+
+public void setCustomerId(int customerId) {
+    this.customerId = customerId;	
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public int getZipCode() {
+	return zipcode;
+}
+
+public void setZipCode(int zipcode) {
+	this.zipcode = zipcode;
+}
+
+public String getCity() {
+	return city;
+}
+
+public void setCity(String city) {
+	this.city = city;
+}
+
+public int getPhoneNo() {
+	return phoneNo;
+}
+
+public void setPhoneNo(int phoneNo) {
+	this.phoneNo = phoneNo;
+}
 
 }
