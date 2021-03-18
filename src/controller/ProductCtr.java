ff85f449588f8616d363edfcb0496a3651e52325
Andreas S W <andreaswilborg@gmail.com>
2021-03-18 17:38:46 +0100

    Controller 2

---
+++ src/controller/ProductCtr.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import db.ProductDB;
+import db.ProductDBIF;
+import db.DataAccessException;
+import model.Product;
+
+public class ProductCtr implements ProductCtrIF {
+	private ProductDBIF ProductDB;
+	
+	public ProductCtr() throws DataAccessException  {
+		try {
+			ProductDB = new ProductDB();
+		} catch (SQLException e) {
+			throw new DataAccessException(e, "Can't create CustomerDB");
+		}
+	}
+	
+	@Override
+	public List<Product> findAll() throws DataAccessException {
+		return ProductDB.findAll();
+	}
+	
+	@Override
+	public Product findByProductNo(int no) throws DataAccessException {
+		return ProductDB.findByProductNo(no);
+	}
+
+}
---
+++ src/db/ProductDB.java
@@ -0,0 +1,81 @@
+package db;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Product;
+
+public class ProductDB implements ProductDBIF {
+	private static final String selectAllQ = 
+			"select purchasePrice, salesPrice, rentPrice, countryOfOrigin, stockAmount, stockStore, stockCar, productNo from groups";
+	private static final String selectByIDQ = 
+			selectAllQ + " where id = ?";
+	private PreparedStatement selectAll; 
+	private PreparedStatement selectByID;
+	
+	public ProductDB() throws SQLException {
+		selectAll = DBConnection.getInstance().getConnection()
+				.prepareStatement(selectAllQ);
+		selectByID = DBConnection.getInstance().getConnection()
+				.prepareStatement(selectByIDQ);
+	}
+	
+	@Override
+	public List<Product> findAll() throws DataAccessException  {
+		try {
+			ResultSet rs = selectAll.executeQuery();
+			List<Product> res = buildObjects(rs);
+			return res;
+		} catch (SQLException e) {
+			DataAccessException he = new DataAccessException(e, "Could not find all");
+			throw he;
+		}
+	}
+
+	@Override
+	public Product findByProductNo(int no) throws DataAccessException {
+		try {
+			selectByID.setInt(1, no);
+			ResultSet rs = selectByID.executeQuery();
+			Product p = null;
+			if(rs.next()) {
+				p = buildObject(rs);
+			}
+			return p;
+		} catch (SQLException e) {
+			throw new DataAccessException(e, "Could not find by id = " + no);
+		}
+	}
+
+	private Product buildObject(ResultSet rs) throws SQLException {
+		Product p = new Product(
+				rs.getString("productName"),
+				rs.getInt("purchasePrice"),
+				rs.getInt("salesPrice"),
+				rs.getInt("rentPrice"),
+				rs.getString("countryOfOrigin"),
+				rs.getInt("stockCar"),
+				rs.getInt("stockStore"),
+				rs.getInt("productNo"),
+				rs.getInt("StockAmount"),
+				rs.getString("storageName")
+				
+				);
+				
+		return p;
+	}
+
+	private List<Product> buildObjects(ResultSet rs) throws SQLException {
+		List<Product> res = new ArrayList<>();
+		while(rs.next()) {
+			res.add(buildObject(rs));
+		}
+		return res;
+	}
+
+}
+
+
